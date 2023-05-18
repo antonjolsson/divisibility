9a2ff5c0b021cd319e6d662750afc54a3872b5eb
Anton J Olsson <anton.j.olsson@bredband.net>
2023-05-19 01:57:49 +0200

    feat: 🎸 divisibility by 1-4, 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,84 @@
-import React, {ReactElement} from 'react';
+import React, {ReactElement, useEffect, useRef, useState} from 'react';
 import './App.css';
 
 function TableRow(props: { entries: string[] }): ReactElement {
     return <div className={'table-row'}>
-        <h2 className={'number'}>{props.entries[0]}</h2>
+        <h2 className={'divisor'}>{props.entries[0]}</h2>
         <h2>{props.entries[1]}</h2>
         <h2 className={'divisible'}>{props.entries[2]}</h2>
     </div>;
 }
 
-function Table(): ReactElement {
+function getDigitSum(number: number): number {
+    return String(number)
+        .split('')
+        .map(s => parseInt(s))
+        .reduce((p, c) => p + c)
+}
+
+function isEvenLongVersion(n: number): boolean {
+    const str = String(n)
+    return [0, 2, 4, 6, 8].includes(parseInt(str[str.length - 1]));
+}
+
+function getNLastDigits(number: number, numOfDigits: number): number {
+    return parseInt(String(number)
+        .split('')
+        .slice(-numOfDigits)
+        .join(''))
+}
+
+function Table(props: {number: number}): ReactElement {
+    useEffect(() => {
+        console.log(props.number)
+    }, [props.number])
+
     const entries = [
         ['rule', 'divisible'],
-        ['Everything', false],
-        ['Digit sum', false],
-        ['Last two digits', false],
+        ['Everything', true],
+        ['Even', isEvenLongVersion(props.number)],
+        ['Digit sum', getDigitSum(props.number) % 3 === 0],
+        ['Last two digits', getNLastDigits(props.number, 2) % 4 === 0],
         ['End in 0 or 5', false],
         ['Divisible by 2 and 3', false],
         ['5 x last + rest', false],
         ['Last 3 digits', false],
-        ['Digit sum', false],
+        ['Digit sum', getDigitSum(props.number) % 9 === 0],
         ['End in 0', false],
         ['Alternating sum', false],
         ['Divisible by 3 and 4', false]
     ]
 
-    return <main>
+    return <div id={'table'}>
         {Array(entries.length).fill(null).map((_, i) => {
-            return <TableRow key={i} entries={[i === 0 ? 'number' : String(i), String(entries[i][0]), String(entries[i][1])]}/>
+            return <TableRow key={i} entries={[i === 0 ? 'divisor' : String(i), String(entries[i][0]), String(entries[i][1])]}/>
         })}
-    </main>;
+    </div>;
+}
+
+function Input(props: {onChange: (n: number) => void}): ReactElement {
+    const inputRef = useRef<HTMLInputElement>(null)
+    useEffect(() => {
+        if (inputRef.current) {
+            props.onChange(parseInt(inputRef.current.value))
+        }
+    }, [])
+    return <div id={'number-input'}>
+        <label>Number to test</label>
+        <input ref={inputRef} type={'number'} defaultValue={27853}
+               onChange={(e): void => props.onChange(parseInt(e.target.value))}/>
+    </div>;
 }
 
 function App(): ReactElement {
+    const [number, setNumber] = useState(-1)
   return (
     <div className="App">
         <img src={'bg.jpg'} alt={'bg'}/>
         <h1>Divisibility</h1>
-        <h3>Easily memorable shortcuts for testing divisibility of 1 to 12</h3>
-        <Table />
+        <h3>Memorable shortcuts for testing divisibility by 1 to 12</h3>
+        <Input onChange={(n: number): void => setNumber(n)}/>
+        <Table number={number}/>
     </div>
   );
 }
