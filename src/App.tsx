45669dd0c24fc411ba562b8ae3778e5b88da7501
Anton J Olsson <anton.j.olsson@bredband.net>
2023-05-21 19:14:54 +0200

    feat: 🎸 show explanations for non-divisors as well

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const BackgroundClickedContext = createContext({bgClicked: false, setBgClicked: 
 const ShowExplanationContext = createContext({showExplanation: false, setShowExplanation: (v: SetStateAction<boolean>) => {}})
 const InfoButtonCoordsContext = createContext({infoButtonCoords: {x: -1, y: -1},
         setInfoButtonCoords: (v: SetStateAction<{x: number, y: number}>) => {}})
-const ExplainedRuleContext = createContext({rule: {number: -1, name: '', divides: false}, setRule: (v: SetStateAction<IRule>) => {}})
+const ExplainedRuleContext = createContext({rule: {divisor: -1, name: '', divides: false}, setRule: (v: SetStateAction<IRule>) => {}})
 
 function TableRow(props: { entries: string[], marked: boolean, markedRank: number }): ReactElement {
     const infoButtonRef = useRef<HTMLImageElement>(null)
@@ -26,7 +26,7 @@ function TableRow(props: { entries: string[], marked: boolean, markedRank: numbe
         showExplanationContext.setShowExplanation(true)
         const rect = infoButtonRef.current?.getBoundingClientRect() ?? {x: -1, y: -1}
         explanationCoordsContext.setInfoButtonCoords({x: rect.x, y: rect.y})
-        setRule({number: parseInt(props.entries[0]), name: props.entries[1], explanation: props.entries[2], divides: true})
+        setRule({divisor: parseInt(props.entries[0]), name: props.entries[1], explanation: props.entries[2], divides: props.marked})
         e.stopPropagation()
     }
 
@@ -94,7 +94,7 @@ function divisibleBy7(number: number): boolean {
 }
 
 export interface IRule {
-    number: number,
+    divisor: number,
     name: string,
     explanation: string
     divides: boolean
@@ -107,34 +107,34 @@ function Table(props: {number: number}): ReactElement {
     useEffect(() => {
         setIsDivisibleByOne(false)
         setTimeout(() => setIsDivisibleByOne(true), 5)
-        setMarkedEntries(rules.filter(e => e.number === 1 || e.divides))
+        setMarkedEntries(rules.filter(e => e.divisor === 1 || e.divides))
     }, [props.number])
 
     const rules: IRule[] = [
-        {number: 1, name: 'Everything', explanation: '', divides: isDivisibleByOne},
-        {number: 2,
-            name: 'Even',
+        {divisor: 1, name: 'Everything', explanation: '', divides: isDivisibleByOne},
+        {divisor: 2,
+            name: 'Even last digit',
             explanation: `A non-negative integer is even if it ends in <b>0, 2, 4, 6</b> or <b>8</b>`,
             divides: isEvenLongVersion(props.number)},
-        {number: 3, name: 'Digit sum', explanation: '', divides: getDigitSum(props.number) % 3 === 0},
-        {number: 4, name: 'Last two digits', explanation: '', divides: divisibleBy4(props.number)},
-        {number: 5, name: 'End in 0 or 5', explanation: '', divides: [0, 5].includes(getNLastDigits(props.number, 1))},
-        {number: 6, name: 'Divisible by 2 and 3', explanation: '', divides: isEvenLongVersion(props.number) && getDigitSum(props.number) % 3 === 0},
-        {number: 7, name: '5 x last + rest', explanation: '', divides: divisibleBy7(props.number)},
-        {number: 8, name: 'Last 3 digits', explanation: '', divides: isEvenLongVersion(getNLastDigits(props.number, 3) / 2 / 2)},
-        {number: 9, name: 'Digit sum', explanation: '', divides: getDigitSum(props.number) % 9 === 0},
-        {number: 10, name: 'End in 0', explanation: '', divides: getNLastDigits(props.number, 1) === 0},
-        {number: 11, name: 'Alternating sum', explanation: '', divides: divisibleBy11(props.number)},
-        {number: 12, name: 'Divisible by 3 and 4', explanation: '', divides: getDigitSum(props.number) % 3 === 0 && divisibleBy4(props.number)}
+        {divisor: 3, name: 'Digit sum', explanation: '', divides: getDigitSum(props.number) % 3 === 0},
+        {divisor: 4, name: 'Last two digits', explanation: '', divides: divisibleBy4(props.number)},
+        {divisor: 5, name: 'End in 0 or 5', explanation: '', divides: [0, 5].includes(getNLastDigits(props.number, 1))},
+        {divisor: 6, name: 'Divisible by 2 and 3', explanation: '', divides: isEvenLongVersion(props.number) && getDigitSum(props.number) % 3 === 0},
+        {divisor: 7, name: '5 x last + rest', explanation: '', divides: divisibleBy7(props.number)},
+        {divisor: 8, name: 'Last 3 digits', explanation: '', divides: isEvenLongVersion(getNLastDigits(props.number, 3) / 2 / 2)},
+        {divisor: 9, name: 'Digit sum', explanation: '', divides: getDigitSum(props.number) % 9 === 0},
+        {divisor: 10, name: 'End in 0', explanation: '', divides: getNLastDigits(props.number, 1) === 0},
+        {divisor: 11, name: 'Alternating sum', explanation: '', divides: divisibleBy11(props.number)},
+        {divisor: 12, name: 'Divisible by 3 and 4', explanation: '', divides: getDigitSum(props.number) % 3 === 0 && divisibleBy4(props.number)}
     ]
 
     return <div id={'table'}>
         {Array(rules.length + 1).fill(null).map((_, i) => {
             const ruleIndex = i - 1
             return <TableRow key={i} marked={i > 0 && rules[ruleIndex].divides}
-                             markedRank={ruleIndex >= 0 ? markedEntries.findIndex(e => e.number === rules[ruleIndex].number) : -1}
+                             markedRank={ruleIndex >= 0 ? markedEntries.findIndex(e => e.divisor === rules[ruleIndex].divisor) : -1}
                              entries={i === 0 ? ['divisor', 'rule', '', 'divisible'] :
-                                 [rules[ruleIndex].number, rules[ruleIndex].name, rules[ruleIndex].explanation,
+                                 [rules[ruleIndex].divisor, rules[ruleIndex].name, rules[ruleIndex].explanation,
                                      rules[ruleIndex].divides].map(v => String(v))}/>
         })}
     </div>;
@@ -159,7 +159,7 @@ function App(): ReactElement {
     const [bgClicked, setBgClicked] = useState(false)
     const [showExplanation, setShowExplanation] = useState(false)
     const [infoButtonCoordsForRule, setInfoButtonCoordsForRule] = useState({x: -1, y: -1})
-    const [ruleExplained, setRuleExplained] = useState({number: -1, name: '', explanation: '', divides: false})
+    const [ruleExplained, setRuleExplained] = useState({divisor: -1, name: '', explanation: '', divides: false})
 
   return (
       <>
--- src/ExplanationWindow.tsx
+++ src/ExplanationWindow.tsx
@@ -1,15 +1,17 @@
 import React, {ReactElement} from "react";
 import {IRule} from "./App";
 
-function Demonstration2(props: {dividend: number}): ReactElement {
+function Demonstration2(props: {dividend: number, divides: boolean}): ReactElement {
     const dividendString = String(props.dividend)
     const firstDigits = dividendString.split('').slice(0, dividendString.length - 1)
     const lastDigit = dividendString.split('').at(-1)
-    return <h1 id={'demonstration2'}>{firstDigits}<span>{lastDigit}</span></h1>;
+    return <h1 id={'demonstration2'} className={props.divides ? 'divisor' : 'not-divisor'}>{firstDigits}
+        <span>{lastDigit}</span></h1>;
 }
 
-function getDemonstration(ruleNumber: number, dividend: number): ReactElement {
-    return <Demonstration2 dividend={dividend}/>
+function getDemonstration(ruleNumber: number, dividend: number, divides: boolean): ReactElement {
+    console.log(divides)
+    return <Demonstration2 dividend={dividend} divides={divides}/>
 }
 
 export function ExplanationWindow(props: { coords: { x: number; y: number }, rule: IRule, dividend: number }): ReactElement {
@@ -20,9 +22,10 @@ export function ExplanationWindow(props: { coords: { x: number; y: number }, rul
     return <div id={'expl-window-container'} style={{left: props.coords.x, top: props.coords.y}}>
         <div id={'expl-window-bg'}></div>
         <div onClick={(e): void => stopPropagation(e)} id={'expl-window'}>
-            <h2 id={'headline'}>{props.rule.name}</h2>
+            <h2 className={'headline'}>{`Divisor: ${props.rule.divisor}`}</h2>
+            <h2 className={'headline'}>{`Rule: ${props.rule.name}`}</h2>
             <div id={'explanation'} dangerouslySetInnerHTML={{ __html: props.rule.explanation }}/>
-            {getDemonstration(props.rule.number, props.dividend)}
+            {getDemonstration(props.rule.divisor, props.dividend, props.rule.divides)}
         </div>
     </div>;
 }
